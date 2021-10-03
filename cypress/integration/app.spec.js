2b396595da8615252bece4eb6cdf18fae57eb041
Cindy Le <hellocindie@gmail.com>
2021-10-03 16:54:03 +1030

    :lipstick:: Changed QRScan to main page (additonally fixed minor visual features)

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,7 +1,7 @@
 describe('Form Validation', () => {
   it('should not submit the form if any field is empty', () => {
     // Start from the index page
-    cy.visit('http://localhost:3000/');
+    cy.visit('http://localhost:3000/uid');
     // Click on the form button
     cy.get('form').submit();
     // Check if the error message is displayed
